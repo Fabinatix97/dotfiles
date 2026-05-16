b1ea16163d160edb477c83a29b8ed8ddc204d144
Fabinatix97 <fabian@fabi-online.de>
2026-05-16 21:59:44 +0200

    Replace neotree with netrw

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,8 +2,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
 
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "80"

355b09feb660c1fbe3b4b8d8b912d8d37eceb167
Fabinatix97 <fabian@fabi-online.de>
2026-06-06 14:30:10 +0200

    Clean up dotfiles structure

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -1,7 +1,7 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function () 
   hl.exec_cmd("waybar & swaync & hypridle")
-  hl.exec_cmd("swaybg -i ~/.config/backgrounds/zelda-and-link.jpg -m fill")
+  hl.exec_cmd("swaybg -i ~/.config/hypr/images/zelda-and-link.jpg -m fill")
   hl.exec_cmd("cliphist wipe")
   hl.exec_cmd("wl-paste --watch cliphist store")
 end)

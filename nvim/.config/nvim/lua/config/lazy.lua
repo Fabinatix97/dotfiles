4f52fe47215d9bafb2070827ddfa4bea83d60fcc
Fabinatix97 <fabian@fabi-online.de>
2026-09-13 07:50:18 +0200

    Disable notifications by lazy update checker

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,10 @@ require("lazy").setup({
 		{ import = "plugins" },
 	},
 	install = { colorscheme = { "habamax" } },
-	checker = { enabled = true },
+	checker = {
+        enabled = true,
+        notify = false,
+    },
 	rocks = { enabled = false },
 	ui = {
 		border = "rounded",
